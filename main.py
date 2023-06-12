3a3abd378a4d7f9d4b37765296dbf8a5111ccefa
Jeremy Hines <107685330+ProfessorKaos62@users.noreply.github.com>
2023-06-12 07:29:27 -0400

    push 2, still trying to install pandas

---
+++ main.py
@@ -0,0 +1,14 @@
+# with open('weather_data.csv', mode='r') as weather_data:
+#     data = weather_data.readlines()
+    
+# import csv
+
+# with open('weather_data.csv', mode='r') as weather_data:
+#     data = csv.reader(weather_data)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != 'temp':
+#             temperatures.append(int(row[1]))
+#     print(temperatures)
+
+import pandas
