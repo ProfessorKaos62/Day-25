74f8b809fffeb66219a793d9930187b4f2780c40
Jeremy <jhines6092@gmail.com>
2023-06-13 12:18:35 -0400

    Finished project

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pandas
 # with open('weather_data.csv', mode='r') as weather_data:
 #     data = weather_data.readlines()
     
@@ -11,5 +12,17 @@
 #             temperatures.append(int(row[1]))
 #     print(temperatures)
 
-import pandas
-print('it worked')
+data = pandas.read_csv('Squirrel_Data.csv')
+fur_color = data['Primary Fur Color']
+fur_list = fur_color.to_list()
+black = fur_list.count('Black')
+cinnamon = fur_list.count('Cinnamon')
+gray = fur_list.count('Gray')
+
+fur_table = {
+    'Color': ['Black', 'Cinnamon', 'Gray'],
+    'Number': [black, cinnamon, gray]
+}
+
+df = pandas.DataFrame(fur_table)
+df.to_csv('Squirrel_Count.csv')
